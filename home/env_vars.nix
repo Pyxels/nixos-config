8fb62293fe6352c3c91796cd27b538123ce8a149
Pyxels <39232833+Pyxels@users.noreply.github.com>
2023-03-21 13:57:13 +0100

    fix(envs): remove unnecessary variables that are set in hyprland conf

--- home/env_vars.nix
+++ home/env_vars.nix
@@ -1,15 +1,4 @@
 {
-  HYPRLAND_LOG_WLR=1;
-
-  # Tell XWayland to use a cursor theme
-  # XCURSOR_THEME=Bibata-Modern-Classic
-  XCURSOR_SIZE=24;
-  XCURSOR_THEME="Breeze_Hacked";
-  
-  # Enable appications
-  MOZ_ENABLE_WAYLAND=1;
-  QT_QPA_PLATFORM="wayland";
-  
   # etc
-  # SUDO_ASKPASS="~/code/scripts/askpass_kickoff.sh"
+  SUDO_ASKPASS="~/code/scripts/askpass_kickoff.sh";
 }
