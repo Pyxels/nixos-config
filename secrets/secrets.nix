8416663b2db3711f7986cd23737d0d6fc1ea0f92
Pyxels <39232833+Pyxels@users.noreply.github.com>
2023-03-22 16:24:17 +0100

    chore(agenix): add new pc creds

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,8 @@ let
   users = [ jonas-l540 ];
 
   nixos-l540 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN1Jl4Nss6OmOtm60n/Jm6/9yk0/O0RKiQe6YMa8LcPn root@nixos-l540";
-  systems = [ nixos-l540 ];
+  vetus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ24D0+0CdFHLHhMGs11dqvOhEnh7xSf7sxzdSpu47Lp root@vetus";
+  systems = [ nixos-l540 vetus ];
 in
 {
   "vpn-credentials.age".publicKeys = users ++ systems;
