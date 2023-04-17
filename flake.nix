6095f5afcb1ba58d78091317e6f238126f91ba30
Pyxels <39232833+Pyxels@users.noreply.github.com>
2023-04-17 09:15:29 +0200

    feat(inputs): switch from nixos-unstable to nixpkgs master branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My first NixOS Config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
