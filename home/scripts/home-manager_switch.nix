dd5d3da263ed4ddb15f2e4de6795b620b9b835d0
Pyxels <39232833+Pyxels@users.noreply.github.com>
2023-10-25 18:52:47 +0200

    feat(scripts): add nom to switch scripts

--- home/scripts/home-manager_switch.nix
+++ home/scripts/home-manager_switch.nix
@@ -2,7 +2,7 @@
 
 pkgs.writeShellScriptBin "hms" ''
 
-  ${pkgs.home-manager}/bin/home-manager switch --flake ${configPath} || exit 1
+  ${pkgs.home-manager}/bin/home-manager switch --flake ${configPath} |& ${pkgs.nix-output-monitor}/bin/nom || exit 1
   previous_config=$(ls -1 --sort=time $NIX_USER_PROFILE_DIR/ | head -n 3 | tail -n 1)
   ${pkgs.nvd}/bin/nvd diff "$NIX_USER_PROFILE_DIR/$previous_config" "$NIX_USER_PROFILE_DIR/profile"
 
--- home/scripts/nixos-rebuild.nix
+++ home/scripts/nixos-rebuild.nix
@@ -2,7 +2,7 @@
 
 pkgs.writeShellScriptBin "nrs" ''
 
-  sudo nixos-rebuild switch --flake ${configPath} || exit 1
+  sudo nixos-rebuild switch --flake ${configPath} |& ${pkgs.nix-output-monitor}/bin/nom || exit 1
   previous_config=$(ls -1 --sort=time /nix/var/nix/profiles/ | head -n 3 | tail -n 1)
   ${pkgs.nvd}/bin/nvd diff "/nix/var/nix/profiles/$previous_config" /nix/var/nix/profiles/system
 
