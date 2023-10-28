13b141284ef6977be402cd7dd131b37009cbacf7
Pyxels <39232833+Pyxels@users.noreply.github.com>
2023-10-28 17:18:09 +0200

    chore(nixos): remove old copied config lines from shared

--- nixos/shared-configuration.nix
+++ nixos/shared-configuration.nix
@@ -82,16 +82,6 @@
   };
 
   nix = {
-    # This will add each flake input as a registry
-    # To make nix3 commands consistent with your flake
-    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-
-    package = pkgs.nixFlakes;
-
-    # This will additionally add your inputs to the system's legacy channels
-    # Making legacy nix commands consistent as well, awesome!
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-
     gc = {
       automatic = true;
       dates = "weekly";
