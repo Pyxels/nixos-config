7863759f6ba455f1e7f5601992ad1c910dc18dbb
Pyxels <39232833+Pyxels@users.noreply.github.com>
2024-05-22 11:36:12 +0200

    feat(flake): add flake parts for devshell and checks

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     agenix = {
       url = "github:ryantm/agenix";
       inputs = {
@@ -18,6 +20,7 @@
     nixvim-config = {
       url = "github:pyxels/nixvim-config";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
     };
 
     hyprland.url = "github:hyprwm/Hyprland";
@@ -30,87 +33,92 @@
       };
     };
 
-    pre-commit-hooks = {
+    git-hooks = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {
-    nixpkgs,
-    home-manager,
-    agenix,
-    ...
-  } @ inputs: let
-    name = "jonas";
-    configPath = "/home/${name}/.dotfiles";
-    hosts = ["vetus" "nixos-l540"];
-    homes = hosts ++ ["jonas-bits"];
+  outputs = {nixpkgs, ...} @ inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      flake = let
+        name = "jonas";
+        configPath = "/home/${name}/.dotfiles";
+        hosts = ["vetus" "nixos-l540"];
+        homes = hosts ++ ["jonas-bits"];
 
-    mkNixosSystems = hosts:
-      builtins.listToAttrs (map
-        (hostName: let
-          host = import ./hosts/${hostName}.nix {};
-        in {
-          name = hostName;
-          value = nixpkgs.lib.nixosSystem {
-            specialArgs = {inherit inputs name host;};
-            modules = [
-              agenix.nixosModules.default
-              ./nixos/${hostName}/configuration.nix
-            ];
+        mkNixosSystems = hosts:
+          builtins.listToAttrs (map
+            (hostName: let
+              host = import ./hosts/${hostName}.nix {};
+            in {
+              name = hostName;
+              value = nixpkgs.lib.nixosSystem {
+                specialArgs = {inherit inputs name host;};
+                modules = [
+                  inputs.agenix.nixosModules.default
+                  ./nixos/${hostName}/configuration.nix
+                ];
+              };
+            })
+            hosts);
+
+        mkHomeConfigs = name: hosts:
+          builtins.listToAttrs (map
+            (hostName: let
+              host = import ./hosts/${hostName}.nix {};
+            in {
+              name = "${name}@${hostName}";
+              value = inputs.home-manager.lib.homeManagerConfiguration {
+                pkgs = nixpkgs.legacyPackages.${host.system}; # Home-manager requires 'pkgs' instance
+                extraSpecialArgs = {inherit inputs name configPath host;};
+                modules = [./home/home.nix] ++ host.modules;
+              };
+            })
+            hosts);
+      in {
+        nixosConfigurations = mkNixosSystems hosts;
+        homeConfigurations = mkHomeConfigs name homes;
+
+        templates = import ./templates;
+      };
+
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        git-hooks = inputs.git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            alejandra.enable = true;
+            flake-checker.enable = true;
+            nil.enable = true;
+            deadnix = {
+              enable = true;
+              settings.exclude = ["./nixos/vetus/hardware-configuration.nix" "./nixos/nixos-l540/hardware-configuration.nix" "./templates/rust-crane/flake.nix"];
+            };
+            statix = {
+              enable = true;
+              settings.ignore = ["**/*hardware-configuration.nix"];
+            };
+
+            shellcheck.enable = true;
+            actionlint.enable = true;
+            commitizen.enable = true;
+            check-merge-conflicts.enable = true;
           };
-        })
-        hosts);
-
-    mkHomeConfigs = name: hosts:
-      builtins.listToAttrs (map
-        (hostName: let
-          host = import ./hosts/${hostName}.nix {};
-        in {
-          name = "${name}@${hostName}";
-          value = home-manager.lib.homeManagerConfiguration {
-            pkgs = nixpkgs.legacyPackages.${host.system}; # Home-manager requires 'pkgs' instance
-            extraSpecialArgs = {inherit inputs name configPath host;};
-            modules = [./home/home.nix] ++ host.modules;
-          };
-        })
-        hosts);
-
-    git-hooks = inputs.pre-commit-hooks.lib.x86_64-linux.run {
-      src = ./.;
-      hooks = {
-        alejandra.enable = true;
-        flake-checker.enable = true;
-        nil.enable = true;
-        deadnix = {
-          enable = true;
-          settings.exclude = ["./nixos/vetus/hardware-configuration.nix" "./nixos/nixos-l540/hardware-configuration.nix" "./templates/rust-crane/flake.nix"];
         };
-        statix = {
-          enable = true;
-          settings.ignore = ["**/*hardware-configuration.nix"];
+      in {
+        devShells.default = pkgs.mkShell {
+          inherit (git-hooks) shellHook;
+          buildInputs = git-hooks.enabledPackages;
         };
 
-        shellcheck.enable = true;
-        actionlint.enable = true;
-        commitizen.enable = true;
-        check-merge-conflicts.enable = true;
+        checks = {
+          inherit git-hooks;
+        };
       };
     };
-  in {
-    nixosConfigurations = mkNixosSystems hosts;
-    homeConfigurations = mkHomeConfigs name homes;
-
-    templates = import ./templates;
-
-    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      inherit (git-hooks) shellHook;
-      buildInputs = git-hooks.enabledPackages;
-    };
-
-    checks.x86_64-linux = {
-      inherit git-hooks;
-    };
-  };
 }
