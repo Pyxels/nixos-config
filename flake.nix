26ed47180cbb2daebd8bae5993f839a4d5f20e8d
Pyxels <39232833+Pyxels@users.noreply.github.com>
2024-07-14 16:10:29 +0200

    refactor(git-hooks): move git hooks to own file

--- flake.nix
+++ flake.nix
@@ -91,27 +91,7 @@
         system,
         ...
       }: let
-        git-hooks = inputs.git-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            alejandra.enable = true;
-            flake-checker.enable = true;
-            nil.enable = true;
-            deadnix = {
-              enable = true;
-              settings.exclude = ["./nixos/vetus/hardware-configuration.nix" "./nixos/nixos-l540/hardware-configuration.nix" "./templates/rust-crane/flake.nix"];
-            };
-            statix = {
-              enable = true;
-              settings.ignore = ["**/*hardware-configuration.nix"];
-            };
-
-            shellcheck.enable = true;
-            actionlint.enable = true;
-            commitizen.enable = true;
-            check-merge-conflicts.enable = true;
-          };
-        };
+        git-hooks = import ./git-hooks.nix {inherit inputs system;};
       in {
         devShells.default = pkgs.mkShell {
           inherit (git-hooks) shellHook;
