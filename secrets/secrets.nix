933f6f80fe210257494fadd0ecea094312669c5f
Pyxels <39232833+Pyxels@users.noreply.github.com>
2024-07-14 15:50:52 +0200

    feat(secrets): add vps host and domain + attic secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,16 @@
 let
   jonas-l540 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII+pzjmsotqutUKGBpYBWyvyH1wQZJ/a4rOHaghaaCG/ jonas@nixos-l540";
   jonas-vetus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPYL1ifWNX2y80O0Sk4OnKZIDJ3Y7dXVRtNcmCkusXuS jonas@vetus";
-  users = [jonas-l540 jonas-vetus];
+  jonas-bits = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILXebXmITS8OF3CLbwVC+HFRg8b/96FiEccvAfQV/ovZ jonas@jonas-bits";
+  users = [jonas-l540 jonas-vetus jonas-bits];
 
   nixos-l540 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN1Jl4Nss6OmOtm60n/Jm6/9yk0/O0RKiQe6YMa8LcPn root@nixos-l540";
   vetus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ24D0+0CdFHLHhMGs11dqvOhEnh7xSf7sxzdSpu47Lp root@vetus";
-  systems = [nixos-l540 vetus];
+  arm-vps = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJLMDZFCA5FqnUaHSkIUt9+7u1f6xUwSwIXZ2j5kDV/c root@arm-vps";
+  systems = [nixos-l540 vetus arm-vps];
 in {
   "vpn-credentials.age".publicKeys = users ++ systems;
   "vpn-config.age".publicKeys = users ++ systems;
+  "domain.age".publicKeys = users ++ systems;
+  "attic.env.age".publicKeys = users ++ systems;
 }
