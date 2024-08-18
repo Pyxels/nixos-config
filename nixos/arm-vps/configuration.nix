91b95a12c970e8352a51164c130b59708a7a3a71
Pyxels <39232833+Pyxels@users.noreply.github.com>
2024-08-18 15:49:37 +0200

    feat(monitoring): add grafana/prometheus/loki/promtail to arm-vps

--- nixos/arm-vps/configuration.nix
+++ nixos/arm-vps/configuration.nix
@@ -70,6 +70,141 @@ in {
     };
   };
 
+  ### MONITORING ###
+  services.prometheus = {
+    port = 3020;
+    enable = true;
+
+    exporters = {
+      node = {
+        port = 3021;
+        enabledCollectors = ["systemd"];
+        enable = true;
+      };
+    };
+
+    scrapeConfigs = [
+      {
+        job_name = "nodes";
+        static_configs = [
+          {
+            targets = [
+              "127.0.0.1:${toString config.services.prometheus.exporters.node.port}"
+            ];
+          }
+        ];
+      }
+    ];
+  };
+
+  services.loki = {
+    enable = true;
+    configuration = {
+      auth_enabled = false;
+
+      server = {
+        http_listen_port = 3030;
+      };
+
+      common = {
+        instance_addr = "127.0.0.1";
+        path_prefix = "/tmp/loki";
+        storage.filesystem = {
+          chunks_directory = "/tmp/loki/chunks";
+          rules_directory = "/tmp/loki/rules";
+        };
+        replication_factor = 1;
+        ring.kvstore.store = "inmemory";
+      };
+
+      query_range.results_cache.cache.embedded_cache = {
+        enabled = true;
+        max_size_mb = 100;
+      };
+
+      schema_config.configs = [
+        {
+          from = "2020-10-24";
+          store = "tsdb";
+          object_store = "filesystem";
+          schema = "v13";
+          index = {
+            prefix = "index_";
+            period = "24h";
+          };
+        }
+      ];
+
+      analytics.reporting_enabled = false;
+    };
+  };
+
+  services.promtail = {
+    enable = true;
+    configuration = {
+      server = {
+        http_listen_port = 3031;
+        grpc_listen_port = 0;
+      };
+      positions = {
+        filename = "/tmp/positions.yaml";
+      };
+      clients = [
+        {
+          url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
+        }
+      ];
+      scrape_configs = [
+        {
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+            };
+          };
+          relabel_configs = [
+            {
+              source_labels = ["__journal__systemd_unit"];
+              target_label = "unit";
+            }
+          ];
+        }
+      ];
+    };
+  };
+
+  services.grafana = {
+    enable = true;
+
+    settings = {
+      server = {
+        protocol = "http";
+        http_port = 3010;
+        http_addr = "127.0.0.1";
+        rootUrl = "https://grafana.${domain}/";
+      };
+      analytics.reporting_enable = false;
+    };
+
+    provision.datasources.settings = {
+      datasources = [
+        {
+          name = "Prometheus";
+          type = "prometheus";
+          access = "proxy";
+          url = "http://127.0.0.1:${toString config.services.prometheus.port}";
+        }
+        {
+          name = "Loki";
+          type = "loki";
+          access = "proxy";
+          url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}";
+        }
+      ];
+    };
+  };
+
   ### REVERSE PROXY ###
   networking.firewall.allowedTCPPorts = [80 443];
   networking.firewall.allowedUDPPorts = [80 443];
@@ -82,11 +217,28 @@ in {
     recommendedTlsSettings = true;
     clientMaxBodySize = "500m";
 
+    upstreams = {
+      "attic".servers."127.0.0.1:8080" = {};
+      "grafana".servers."127.0.0.1:${toString config.services.grafana.settings.server.http_port}" = {};
+      "prometheus".servers."127.0.0.1:${toString config.services.prometheus.port}" = {};
+      "loki".servers."127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}" = {};
+      "promtail".servers."127.0.0.1:${toString config.services.promtail.configuration.server.http_listen_port}" = {};
+    };
+
     virtualHosts.${atticDomain} = {
       enableACME = true;
       forceSSL = true;
       locations."/" = {
-        proxyPass = "http://127.0.0.1:8080";
+        proxyPass = "http://attic";
+      };
+    };
+
+    virtualHosts."grafana.${domain}" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://grafana";
+        proxyWebsockets = true;
       };
     };
   };
