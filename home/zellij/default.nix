b76f914b66c5478308fd3b783695d82a0d288519
Pyxels <39232833+Pyxels@users.noreply.github.com>
2024-08-28 11:55:08 +0200

    feat(zellij): reduce ui space with compact-bar and use gruvbox

--- home/zellij/default.nix
+++ home/zellij/default.nix
@@ -1,12 +1,18 @@
 _: {
   programs.zellij.enable = true;
 
-  xdg.configFile.zellij = {
-    target = "zellij/config.kdl";
-    source = ./zellij.kdl;
-  };
-  xdg.configFile.zellij_layout = {
-    target = "zellij/layouts/default.kdl";
-    source = ./default_layout.kdl;
+  xdg.configFile = {
+    zellij_config = {
+      target = "zellij/config.kdl";
+      source = ./zellij.kdl;
+    };
+    zellij_layout = {
+      target = "zellij/layouts/default.kdl";
+      source = ./default_layout.kdl;
+    };
+    zellij_theme = {
+      target = "zellij/themes/gruvbox.kdl";
+      source = ./gruvbox_with_selection.kdl;
+    };
   };
 }
