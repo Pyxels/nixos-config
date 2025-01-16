e0da9c59a79ec9647d6ba49e2111f325d365c24d
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-01-16 13:46:10 +0100

    feat(arm-vps): add beszel hub and agent

--- nixos/arm-vps/configuration.nix
+++ nixos/arm-vps/configuration.nix
@@ -249,6 +249,37 @@ in {
     };
   };
 
+  ### BESZEL ###
+  systemd.services = {
+    beszel-hub = {
+      description = "Beszel Hub";
+      after = ["network.target"];
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = "3";
+        User = "root";
+        WorkingDirectory = "/root/beszel";
+        ExecStart = "${pkgs.beszel}/bin/beszel-hub serve --http '0.0.0.0:8090'";
+      };
+      wantedBy = ["multi-user.target"];
+    };
+    beszel-agent = {
+      description = "Beszel Agent Service";
+      after = ["network.target"];
+      serviceConfig = {
+        Restart = "always";
+        RestartSec = "5";
+        ExecStart = "${pkgs.beszel}/bin/beszel-agent";
+      };
+      environment = {
+        PORT = "45876";
+        KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8LG4ijPENqiHz1kS/2+CVZXKgl4nmlY0xCJPuMLhcU";
+      };
+      wantedBy = ["multi-user.target"];
+    };
+  };
+
   ### REVERSE PROXY ###
   networking.firewall.allowedTCPPorts = [80 443];
   networking.firewall.allowedUDPPorts = [80 443];
