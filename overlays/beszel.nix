d768a446e6253daf9c921bd7c386891e6d324877
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-03-17 09:36:45 +0100

    chore(beszel): update to v0.10.2

--- overlays/beszel.nix
+++ overlays/beszel.nix
@@ -1,15 +1,15 @@
 (final: prev: {
   beszel = (prev.beszel.override {buildGoModule = final.buildGo124Module;}).overrideAttrs (oldAttrs: rec {
-    version = "0.10.1";
+    version = "0.10.2";
     src = oldAttrs.src.override {
       tag = "v${version}";
-      hash = "sha256-4RuYZcBR7X9Ug6l91N/FtyfT38HlW2guputzo4kF8YU=";
+      hash = "sha256-yYSX58qA4vE7Bp3ADc6rIMf9yaeU7Zw7D5rmES6x6oA=";
     };
     webui = oldAttrs.webui.overrideAttrs (oldWebui: rec {
       inherit src version;
       npmDeps = oldWebui.npmDeps.overrideAttrs (_: {
         inherit src;
-        outputHash = "sha256-UKOS7QyGsdKosjhxVhZErFkXhnfrFxdX0ozBUJGsNII=";
+        outputHash = "sha256-27NUV23dNHFSwOHiB/wGSAWkp6eZMnw/6Pd3Fwn98+s=";
         name = "${oldAttrs.pname}-${version}-npm-deps";
       });
     });
