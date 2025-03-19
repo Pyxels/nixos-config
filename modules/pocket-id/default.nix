4d23c6cf52478ba31111f713e86233e5c47b47c6
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-03-19 07:17:15 +0100

    chore(pocket-id): update to v0.42.1

--- modules/pocket-id/default.nix
+++ modules/pocket-id/default.nix
@@ -42,7 +42,7 @@ in {
     };
 
     virtualisation.oci-containers.containers."pocket-id" = {
-      image = "ghcr.io/pocket-id/pocket-id:v0.40.1";
+      image = "ghcr.io/pocket-id/pocket-id:v0.42.1";
       ports = ["127.0.0.1:${toString cfg.port}:8100"];
       volumes = ["${cfg.stateDir}:/app/backend/data"];
       environment = {
