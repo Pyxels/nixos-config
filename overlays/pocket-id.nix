3e494f077f158eb40cafcd9883a5d2236ff820d8
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-06-23 09:54:49 +0200

    chore(pocket-id): update to v1.4.1

--- overlays/pocket-id.nix
+++ overlays/pocket-id.nix
@@ -1,11 +1,11 @@
 (final: prev: {
   pocket-id = prev.pocket-id.overrideAttrs (oldAttrs: rec {
-    version = "1.4.0";
+    version = "1.4.1";
     src = final.fetchFromGitHub {
       owner = "pocket-id";
       repo = "pocket-id";
       tag = "v${version}";
-      sha256 = "sha256-2wwqvxr1YmfMFhfmx8rTClv+2ug493rl/L/xklEHnUw=";
+      sha256 = "sha256-sKQ+f68djyuCRdr2x73QNiN9wbmnsTCmHyg/o1YvwHI=";
     };
     vendorHash = "sha256-9zPajwpfjsItAx3WAk4JiVlraTGHmYhLJ6VKRulPFOI=";
 
