9116fe70c18442520dcfd40344a0e41cfa9cdeb9
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-06-30 10:18:34 +0200

    chore(arm-vps): bump pocket-id to 1.5.0

--- overlays/pocket-id.nix
+++ overlays/pocket-id.nix
@@ -1,11 +1,11 @@
 (final: prev: {
   pocket-id = prev.pocket-id.overrideAttrs (oldAttrs: rec {
-    version = "1.4.1";
+    version = "1.5.0";
     src = final.fetchFromGitHub {
       owner = "pocket-id";
       repo = "pocket-id";
       tag = "v${version}";
-      sha256 = "sha256-sKQ+f68djyuCRdr2x73QNiN9wbmnsTCmHyg/o1YvwHI=";
+      sha256 = "sha256-U5tZq310A6hnDIG8CeJl58EWLSshbXVaKJGSzLUCMI4=";
     };
     vendorHash = "sha256-9zPajwpfjsItAx3WAk4JiVlraTGHmYhLJ6VKRulPFOI=";
 
@@ -13,7 +13,7 @@
       inherit src version;
       npmDeps = oldFrontend.npmDeps.overrideAttrs (_: {
         inherit src;
-        outputHash = "sha256-Ge3+IaffW8jCfUwj2FOt0GR0HbVy70EXel9TN2zVrB4=";
+        outputHash = "sha256-AZ8je9uaJ1h9wxfs2RtPr2Ki0QNYD0nDd2BZDj6/sl8=";
       });
     });
   });
