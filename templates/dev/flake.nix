86a21463442216fe4ee6c2e3ca78ab1c49119d00
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-07-02 11:13:53 +0200

    chore(templates): formatting and gitignore for basic dev shell

--- templates/dev/flake.nix
+++ templates/dev/flake.nix
@@ -8,7 +8,9 @@
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {pkgs, ...}: {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [bashInteractive];
+          packages = with pkgs; [
+            bashInteractive
+          ];
         };
       };
     };
