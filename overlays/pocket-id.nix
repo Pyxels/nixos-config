a098cab46a88f5060cd8e26a4f5bb9f2a0103873
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-07-21 09:53:54 +0200

    chore(arm-vps): bump pocket-id to v1.6.4

--- overlays/pocket-id.nix
+++ overlays/pocket-id.nix
@@ -1,19 +1,19 @@
 (final: prev: {
   pocket-id = prev.pocket-id.overrideAttrs (oldAttrs: rec {
-    version = "1.6.2";
+    version = "1.6.4";
     src = final.fetchFromGitHub {
       owner = "pocket-id";
       repo = "pocket-id";
       tag = "v${version}";
-      sha256 = "sha256-fg9iT4JGB3CvmPiRaQwfyKxZ5T0mweDQAQYYU/fdb/g=";
+      sha256 = "sha256-P6pA0760eo/dL1t5Jics4oSztM4F/C8lIuZ3dZ9x5C8=";
     };
-    vendorHash = "sha256-LutjhewhizxGc/YlNHpK81HrX+wSAAJLWtA+skTjn1w=";
+    vendorHash = "sha256-8D7sSmxR+Fq4ouB9SuoEDplu6Znv3U0BIyYISSmF6Bs=";
 
     frontend = oldAttrs.frontend.overrideAttrs (oldFrontend: {
       inherit src version;
       npmDeps = oldFrontend.npmDeps.overrideAttrs (_: {
         inherit src;
-        outputHash = "sha256-AZ8je9uaJ1h9wxfs2RtPr2Ki0QNYD0nDd2BZDj6/sl8=";
+        outputHash = "sha256-FiFSnN6DOMr8XghvyGTWB/EMTNfvpqlAgx7FPnbGQxU=";
       });
     });
   });
