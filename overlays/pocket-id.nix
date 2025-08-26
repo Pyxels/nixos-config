095b33904705361ce95c1efa8ce1727916d15d0e
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-08-26 10:35:14 +0200

    chore(pocket-id): bump to v1.9.1

--- overlays/pocket-id.nix
+++ overlays/pocket-id.nix
@@ -1,53 +1,21 @@
 (final: prev: {
-  pocket-id = prev.pocket-id.overrideAttrs (_oldAttrs: rec {
-    version = "1.7.0";
-    src = final.fetchFromGitHub {
-      owner = "pocket-id";
-      repo = "pocket-id";
-      tag = "v${version}";
-      sha256 = "sha256-u4H1wC5RL3p7GNL7WQkmK8DNgwKQvgxHd8TIug+Be+o=";
-    };
-    vendorHash = "sha256-guG/JnwUi2WeClSfAX9pRG3kLJMTvTDiJ7L54TGeSd0=";
-
-    frontend = final.stdenv.mkDerivation (finalAttrs: {
-      pname = "pocket-id-frontend";
-      inherit version src;
-
-      nativeBuildInputs = [
-        final.nodejs
-        final.pnpm.configHook
-      ];
-
-      pnpmWorkspaces = ["pocket-id-frontend"];
-      pnpmDeps = final.pnpm.fetchDeps {
-        inherit
-          (finalAttrs)
-          pname
-          version
-          src
-          pnpmWorkspaces
-          ;
-        fetcherVersion = 1;
-        hash = "sha256-GSlyctAq7ubll9MtYy0lrAj9hh/bdWrQFh2fCu/dWuw=";
+  pocket-id =
+    (prev.pocket-id.override {buildGoModule = final.buildGo125Module;}).overrideAttrs
+    (oldAttrs: rec {
+      version = "1.9.1";
+      src = final.fetchFromGitHub {
+        owner = "pocket-id";
+        repo = "pocket-id";
+        tag = "v${version}";
+        sha256 = "sha256-3sUkEbC96/XUR1tvCmxu56hPh7Ag/sD6/pGrq9JhHC8=";
       };
+      vendorHash = "sha256-eNUhk76YLHtXCFaxiavM6d8CMeE+YQ+vOecDUCiTh5k=";
 
-      env.BUILD_OUTPUT_PATH = "dist";
-      buildPhase = ''
-        runHook preBuild
-
-        pnpm --filter pocket-id-frontend run build
-
-        runHook postBuild
-      '';
-
-      installPhase = ''
-        runHook preInstall
-
-        mkdir -p $out/lib/pocket-id-frontend
-        cp -r frontend/dist $out/lib/pocket-id-frontend/dist
-
-        runHook postInstall
-      '';
+      frontend = oldAttrs.frontend.overrideAttrs (prev: {
+        inherit version src;
+        pnpmDeps = prev.pnpmDeps.overrideAttrs (_: {
+          outputHash = "sha256-q2oXyFVdaDfJ4NFDt26/VJVXzQLCuKXHtCx1mah6Js8=";
+        });
+      });
     });
-  });
 })
