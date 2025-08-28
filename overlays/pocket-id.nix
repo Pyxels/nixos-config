6a759da7ba54d866ce9c13e6c4f9473584c8e12c
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-08-28 08:44:53 +0200

    chore(pocket-id): bump to v1.10.0

--- overlays/pocket-id.nix
+++ overlays/pocket-id.nix
@@ -2,19 +2,19 @@
   pocket-id =
     (prev.pocket-id.override {buildGoModule = final.buildGo125Module;}).overrideAttrs
     (oldAttrs: rec {
-      version = "1.9.1";
+      version = "1.10.0";
       src = final.fetchFromGitHub {
         owner = "pocket-id";
         repo = "pocket-id";
         tag = "v${version}";
-        sha256 = "sha256-3sUkEbC96/XUR1tvCmxu56hPh7Ag/sD6/pGrq9JhHC8=";
+        sha256 = "sha256-YAQT7ORRg27ORh57NTE8F89iNfw+3gd1xPM8f4zHKm4=";
       };
       vendorHash = "sha256-eNUhk76YLHtXCFaxiavM6d8CMeE+YQ+vOecDUCiTh5k=";
 
       frontend = oldAttrs.frontend.overrideAttrs (prev: {
         inherit version src;
         pnpmDeps = prev.pnpmDeps.overrideAttrs (_: {
-          outputHash = "sha256-q2oXyFVdaDfJ4NFDt26/VJVXzQLCuKXHtCx1mah6Js8=";
+          outputHash = "sha256-Gjj2iFQ15Tso0gXihFH96nW49GJleOU323shBE7VgJ4=";
         });
       });
     });
