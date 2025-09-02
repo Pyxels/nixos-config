ad1bd7ab18d4ef0664bfa5ec7dfb44163d7ee839
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-09-02 09:14:59 +0200

    chore(flake): update all inputs

--- overlays/pocket-id.nix
+++ overlays/pocket-id.nix
@@ -1,21 +1,19 @@
 (final: prev: {
-  pocket-id =
-    (prev.pocket-id.override {buildGoModule = final.buildGo125Module;}).overrideAttrs
-    (oldAttrs: rec {
-      version = "1.10.0";
-      src = final.fetchFromGitHub {
-        owner = "pocket-id";
-        repo = "pocket-id";
-        tag = "v${version}";
-        sha256 = "sha256-YAQT7ORRg27ORh57NTE8F89iNfw+3gd1xPM8f4zHKm4=";
-      };
-      vendorHash = "sha256-eNUhk76YLHtXCFaxiavM6d8CMeE+YQ+vOecDUCiTh5k=";
+  pocket-id = prev.pocket-id.overrideAttrs (oldAttrs: rec {
+    version = "1.10.0";
+    src = final.fetchFromGitHub {
+      owner = "pocket-id";
+      repo = "pocket-id";
+      tag = "v${version}";
+      sha256 = "sha256-YAQT7ORRg27ORh57NTE8F89iNfw+3gd1xPM8f4zHKm4=";
+    };
+    vendorHash = "sha256-eNUhk76YLHtXCFaxiavM6d8CMeE+YQ+vOecDUCiTh5k=";
 
-      frontend = oldAttrs.frontend.overrideAttrs (prev: {
-        inherit version src;
-        pnpmDeps = prev.pnpmDeps.overrideAttrs (_: {
-          outputHash = "sha256-Gjj2iFQ15Tso0gXihFH96nW49GJleOU323shBE7VgJ4=";
-        });
+    frontend = oldAttrs.frontend.overrideAttrs (prev: {
+      inherit version src;
+      pnpmDeps = prev.pnpmDeps.overrideAttrs (_: {
+        outputHash = "sha256-Gjj2iFQ15Tso0gXihFH96nW49GJleOU323shBE7VgJ4=";
       });
     });
+  });
 })
