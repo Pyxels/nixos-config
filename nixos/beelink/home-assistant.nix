429d9d937cb2bc9b9b4242e415161df309777a2b
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-09-27 11:55:30 +0200

    feat(beelink): add esphome

--- nixos/beelink/home-assistant.nix
+++ nixos/beelink/home-assistant.nix
@@ -1,12 +1,23 @@
 {
-  virtualisation.oci-containers.containers.homeassistant = {
-    autoStart = true;
-    volumes = ["home-assistant:/config"];
-    environment.TZ = "Europe/Berlin";
-    image = "ghcr.io/home-assistant/home-assistant:2025.9.4";
-    extraOptions = [
-      "--network=host"
-      "--privileged"
-    ];
+  virtualisation.oci-containers.containers = {
+    homeassistant = {
+      autoStart = true;
+      volumes = ["home-assistant:/config"];
+      environment.TZ = "Europe/Berlin";
+      image = "ghcr.io/home-assistant/home-assistant:2025.9.4";
+      extraOptions = [
+        "--network=host"
+        "--privileged"
+      ];
+    };
+    esphome = {
+      autoStart = true;
+      volumes = ["/var/lib/esp-home:/config"];
+      environment.TZ = "Europe/Berlin";
+      image = "ghcr.io/esphome/esphome:2025.9.1";
+      extraOptions = [
+        "--network=host"
+      ];
+    };
   };
 }
