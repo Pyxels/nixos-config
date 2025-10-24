1b7a625dee61918f2b48dda76623d44843fad9b9
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-10-24 15:41:22 +0200

    chore(pocket-id): bump version to 1.14.0

--- overlays/pocket-id.nix
+++ overlays/pocket-id.nix
@@ -1,18 +1,18 @@
 (final: prev: {
   pocket-id = prev.pocket-id.overrideAttrs (oldAttrs: rec {
-    version = "1.13.1";
+    version = "1.14.0";
     src = final.fetchFromGitHub {
       owner = "pocket-id";
       repo = "pocket-id";
       tag = "v${version}";
-      sha256 = "sha256-ayMPyc90bnkIhYfBQ8SgcKayjUQSyrcp5SOLGHTKQwE=";
+      sha256 = "sha256-5F3XchB3kl54Sm45yD9giRd1K3xwPNhFAVYILyY3f2Q=";
     };
-    vendorHash = "sha256-+HF1zAWA6Ak7uJqWCcTXrttTy1sPA8bN+/No95eqFTU=";
+    vendorHash = "sha256-CmhPURPNwcpmD9shLrQPVKFGBirEMjq0Z4lmgMCpxS8=";
 
     frontend = oldAttrs.frontend.overrideAttrs (prev: {
       inherit version src;
       pnpmDeps = prev.pnpmDeps.overrideAttrs (_: {
-        outputHash = "sha256-IVrp5qWYMgud9ryLidrUowWWBHZ2lMrJp0cfPPHpXls=";
+        outputHash = "sha256-/e1zBHdy3exqbMvlv0Jth7vpJd7DDnWXGfMV+Cdr56I=";
       });
     });
   });
